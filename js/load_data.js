459e7a20c0309fe2be8762a12d60cc6221a26fbc
Dereck Santander <disantan@espol.edu.ec>
2023-11-14 23:45:08 -0500

    Update

--- js/load_data.js
+++ js/load_data.js
@@ -1,3 +1,6 @@
+let xml = ''
+let responseTextJS = ''
+
 let parseXML = async () => {
 
     //RESPONSE XML
@@ -8,34 +11,36 @@ let parseXML = async () => {
     //RESPONSE JSON
     let urlJS = "https://raw.githubusercontent.com/DAWMFIEC/DAWM-apps/datos/bookstore-images.json"
     let responseJS = await fetch(urlJS)
-    let responseTextJS = await responseJS.json()
+    responseTextJS = await responseJS.json()
 
     //CARGAR XML
     const parser = new DOMParser();
-    const xml = parser.parseFromString(responseText, "application/xml");
+    xml = parser.parseFromString(responseText, "application/xml");
     console.log(xml)
 
-    renderizarLibros(xml, responseTextJS)
+    renderizarLibros(xml, responseTextJS,"Sin filtro")
 }
 
-let renderizarLibros = (xml, responseTextJS) => {
+let renderizarLibros = (xml, responseTextJS, filter) => {
+
     let actividadElement = document.querySelector("#actividad")
     actividadElement.innerHTML = ''
 
     let bookArr = xml.querySelectorAll("book")
 
-    bookArr.forEach(book => {
-        let book_author = book.querySelector("Book-Author").textContent
-        let year = book.querySelector("Year-Of-Publication").textContent
-        let book_title = book.querySelector("Book-Title").textContent
-        let imgXML = book.querySelector("ISBN").textContent
-        let imgNameJS = ''
-        responseTextJS.forEach(bookJS => {
-            if (imgXML == bookJS["ISBN"]) {
-                imgNameJS = bookJS["Image-URL-M"];
-            }
-        })
-        let plantilla = `
+    if (filter == 'Sin filtro') {
+        bookArr.forEach(book => {
+            let book_author = book.querySelector("Book-Author").textContent
+            let year = book.querySelector("Year-Of-Publication").textContent
+            let book_title = book.querySelector("Book-Title").textContent
+            let imgXML = book.querySelector("ISBN").textContent
+            let imgNameJS = ''
+            responseTextJS.forEach(bookJS => {
+                if (imgXML == bookJS["ISBN"]) {
+                    imgNameJS = bookJS["Image-URL-M"];
+                }
+            })
+            let plantilla = `
             <div class="col-lg-2 mb-2 text-center">
                 <div class="card border-0 rounded-0">
                     <div class="card-image">
@@ -53,13 +58,52 @@ let renderizarLibros = (xml, responseTextJS) => {
                 </div>
             </div>`
 
-        actividadElement.innerHTML += plantilla;
+            actividadElement.innerHTML += plantilla;
+        }
+        )
+    } else {
+        bookArr.forEach(book => {
+            let book_author = book.querySelector("Book-Author").textContent+''
+            let year = book.querySelector("Year-Of-Publication").textContent+''
+            let book_title = book.querySelector("Book-Title").textContent+''
+            let imgXML = book.querySelector("ISBN").textContent
+            let imgNameJS = ''
+            responseTextJS.forEach(bookJS => {
+                if (imgXML == bookJS["ISBN"]) {
+                    imgNameJS = bookJS["Image-URL-M"];
+                }
+            })
+            if ((book_author.includes(filter)) || (year.includes(filter)) || (book_title.includes(filter))) {
+                let plantilla = `
+                    <div class="col-lg-2 mb-2 text-center">
+                        <div class="card border-0 rounded-0">
+                            <div class="card-image">
+                                <img src= ${imgNameJS} alt="blog-img" class="img-fluid">
+                            </div>
+                        </div>
+                        <div class="card-body text-capitalize">
+                            <div class="card-meta fs-6">
+                                <span class="meta-date"> ${book_author} </span>
+                                <span class="meta-category">/ <a href="blog.html"> ${year} </a></span>
+                            </div>
+                            <h4 class="card-title">
+                                <a href="buy.html"> ${book_title} </a>
+                            </h4>
+                        </div>
+                    </div>`
+
+                actividadElement.innerHTML += plantilla;
+            }
+
+        }
+        )
     }
-)}
+}
 
 let searchBook = () => {
-    let selectedBook = document.getElementById("newsletter1").value
-    console.log(selectedBook)
+    let selectedFilter = document.getElementById("newsletter1").value
+    renderizarLibros(xml,responseTextJS,selectedFilter)
+    console.log(selectedFilter)
 }
 
 let loadBooks = () => {
